68a0c10a2098267c3cf113b9a052e2265e7e2973
saulcm555 <saulcastrocm@hotmail.com>
2025-07-22 22:50:44 -0500

    Actualización: pruebas Cypress e interfaz

--- cypress/e2e/productos.cy.js
+++ cypress/e2e/productos.cy.js
@@ -1,17 +1,17 @@
 describe('API Productos', () => {
-    it('Debe obtener la lista de productos', () => {
-      cy.request('http://localhost:3000/productos').then((res) => {
-        expect(res.status).to.eq(200);
-      });
-    });
-  
-    it('Debe agregar un producto nuevo', () => {
-      cy.request('POST', 'http://localhost:3000/productos', {
-        nombre: 'Audífonos'
-      }).then((res) => {
-        expect(res.status).to.eq(201);
-        expect(res.body.nombre).to.eq('Audífonos');
-      });
+  it('Debe obtener lista de productos', () => {
+    cy.request('http://localhost:3000/productos').then((res) => {
+      expect(res.status).to.eq(200);
     });
   });
-  
+
+  it('Debe agregar un producto nuevo', () => {
+    cy.request('POST', 'http://localhost:3000/productos', {
+      nombre: 'Monitor'
+    }).then((res) => {
+      expect(res.status).to.eq(201);
+      expect(res.body.nombre).to.eq('Monitor');
+    });
+  });
+});
+
